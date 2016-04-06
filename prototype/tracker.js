7c7c3f48cfa7c8c0397255e094a9d764cbe9a129
Kathy Wu <kaixi.w@gmail.com>
2016-04-06 01:25:49 -0400

    removed unnecessary code/divs

--- prototype/tracker.js
+++ prototype/tracker.js
@@ -1,27 +1,7 @@
 $('.tracker-content').click(function(){
-  trackerMapper[this.id]();
+  $("#darken").toggleClass("active")
+  $('#'+this.id+'-popup').toggleClass("active");
 });
-
-var thoughts = function() {
-  $("#darken").toggleClass("active")
-  $('#thought-popup').toggleClass("active");
-};
-
-var weight = function() {
-  $("#darken").toggleClass("active")
-  $('#weight-popup').toggleClass("active");
-};
-
-var calories = function() {
-  $("#darken").toggleClass("active")
-  $('#calories-popup').toggleClass("active");
-};
-
-var trackerMapper = {
-  "thoughts": thoughts,
-  "weight": weight,
-  "calories": calories,
-}
 
 $("#darken, .popup-wrapper").click(function(e){
   console.log(e.target);
