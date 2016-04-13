49d5d6f98dd0a284bac786c963eab61bfcc226f5
Jackie Liu <jlliu@mit.edu>
2016-04-13 18:41:42 -0400

    additional styling

--- prototype/tracker.js
+++ prototype/tracker.js
@@ -15,13 +15,13 @@ $('.tracker-content').click(function(){
       datasets: [
         {
           label: "My Weight",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
+          fillColor: "rgba(225,105,92,0.2)",
+          strokeColor: "rgba(225,105,92,1)",
+          pointColor: "rgba(225,105,92,1)",
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [136, 142, 139, 147, 145, 141],
+          pointHighlightStroke: "rgba(225,105,92,1)",
+          data: [136, 142, 140, 147, 145, 149],
         },
       ]
     };
@@ -36,12 +36,12 @@ $('.tracker-content').click(function(){
       datasets: [
         {
           label: "My Calorie",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
+          fillColor: "rgba(225,105,92,0.2)",
+          strokeColor: "rgba(225,105,92,1)",
+          pointColor: "rgba(225,105,92,1)",
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
+          pointHighlightStroke: "rgba(225,105,92,1)",
           data: [1900, 2100, 2400, 1900, 2250, 2200],
         },
       ]
@@ -83,8 +83,28 @@ $("#submitWeight").click(function(){
     weightLineChart.destroy();
     var ctx = document.getElementById("weightChart").getContext("2d");
     weightLineChart = new Chart(ctx).Line(weightData, weightOptions);
+    $("#weightSubmitArea").css("display","none");
+    $("#afterWeightSubmit").css("display","block");
   }
 })
+
+//Also submit weight when pressing enter in weight textbox
+$('#weightTextarea').keydown(function (event) {
+    var keypressed = event.keyCode || event.which;
+    if (keypressed == 13) {
+          if ($("#weightTextarea").val() != ""){
+            $("#weightTextarea").val("");
+            weightData.datasets[0].data.push(146);
+            weightLineChart.destroy();
+            var ctx = document.getElementById("weightChart").getContext("2d");
+            weightLineChart = new Chart(ctx).Line(weightData, weightOptions);
+            $("#weightSubmitArea").css("display","none");
+            $("#afterWeightSubmit").css("display","block");
+        }
+    }
+});
+
+
 
 $("#submitCalorie").click(function(){
   if ($("#CalorieTextarea").val() != ""){
@@ -100,13 +120,13 @@ $("#similarCheckBox").click(function(){
   if($(this).is(':checked')) {
     weightData.datasets.push({
       label: "Similar User",
-      fillColor: "rgba(173,102,31,0.2)",
-      strokeColor: "rgba(173,102,31,1)",
-      pointColor: "rgba(173,102,31,1)",
+      fillColor: "rgba(140,49,93,0.2)",
+      strokeColor: "rgba(140,49,93,1)",
+      pointColor: "rgba(140,49,93,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(173,102,31,1)",
-      data: [135, 133, 140, 135, 138, 142, 145],
+      pointHighlightStroke: "rgba(140,49,93,1)",
+      data: [135, 133, 134, 135, 138, 142, 145],
     });
   } else {
     if (weightData.datasets[1].label == 'Similar User') {
@@ -124,13 +144,13 @@ $("#previousCheckBox").click(function(){
   if($(this).is(':checked')) {
     weightData.datasets.push({
       label: "Previous Pregnancy",
-      fillColor: "rgba(111,51,232,0.2)",
-      strokeColor: "rgba(111,51,232,1)",
-      pointColor: "rgba(111,51,232,1)",
+      fillColor: "rgba(219, 18, 0,0.2)",
+      strokeColor: "rgba(219, 18, 0,1)",
+      pointColor: "rgba(219, 18, 0,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(111,51,232,1)",
-      data: [130, 131, 135, 132, 134, 136, 135],
+      pointHighlightStroke: "rgba(219, 18, 0,1)",
+      data: [130, 131, 132, 132, 134, 136, 135],
     });
   } else {
     if (weightData.datasets[1].label == 'Previous Pregnancy') {
