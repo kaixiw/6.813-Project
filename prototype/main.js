cc5c0ca8db9dc111ab94133c8e90900db0b386f8
hairuo <hairuo@mit.edu>
2016-04-25 22:32:55 -0400

    Todo list partially implemented.

--- prototype/main.js
+++ prototype/main.js
@@ -14,6 +14,18 @@ $(document).ready(function(){
         $(this).attr('placeholder', "Enter reminder here");
     });
     $("#remindersInput").addClass("remindersInputBox");
+    $("#todosInput").keypress(function(event){
+        if (event.keyCode == 13){
+            susan.save(FIELD_TODOS, "todosInput", true);
+            $("#todosInput").val("");
+        }
+    });
+    $("#todosInput").on('blur', function(){
+        $(this).val("");
+        $(this).attr('placeholder', "Enter Todo here");
+    });
+    $("#todosInput").addClass("todosInputBox");
+
     console.log("document loading complete");
 
 });
